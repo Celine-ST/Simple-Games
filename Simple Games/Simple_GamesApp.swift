e0a330218ebb9246050502cef86536ed6b907999
Celine-ST <celine.quek10@rafflesgirlssch.edu.sg>
2023-11-27 15:11:27 +0800

    Initial Commit

---
+++ Simple Games/Simple_GamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_GamesApp.swift
+//  Simple Games
+//
+//  Created by Celine Quek on 27/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Simple_GamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
