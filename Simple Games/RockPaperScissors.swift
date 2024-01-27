2b01412d1cc7b73b171ab9605eacbf51b0fe5a88
Celine-ST <celine.quek10@rafflesgirlssch.edu.sg>
2024-01-27 13:43:32 +0800

    finished the rock paper scissors yayyyy

--- Simple Games/RockPaperScissors.swift
+++ Simple Games/RockPaperScissors.swift
@@ -9,71 +9,132 @@ import SwiftUI
 
 struct RockPaperScissors: View {
     
-    @State private var playerChoice = "rock"
-    @State private var choiceIsFinalised = false
+    enum Sign {
+        case rock, paper, scissors, none
+    }
+    
+    enum Result {
+        case win, draw, lose, picking
+    }
+    
+    @State private var playerChoice = Sign.none
+    @State private var comChoice = Sign.none
+    @State private var event: Result = .picking
+    @State private var buttonText = "I'm Ready"
     
     var body: some View {
         VStack {
+                VStack {
+                    Image(systemName: comChoice == .rock ? "circle.fill" : comChoice == .paper ? "square.fill" : comChoice == .scissors ? "triangle.fill" : "questionmark")
+                    Text(comChoice == .rock ? "Rock" : comChoice == .paper ? "Paper" : comChoice == .scissors ? "Scissors" : "???")
+                }
+                .frame(width: 100, height: 75)
+                .background(comChoice == .rock ? .red : comChoice == .paper ? .yellow : comChoice == .scissors ? .blue : .purple)
+                .cornerRadius(30)
+                .fontWeight(.bold)
+                .foregroundStyle(Color(.white))
             HStack {
-                if !choiceIsFinalised || playerChoice == "rock" {
+                if event == .picking || playerChoice == .rock {
                     Button() {
-                        playerChoice = "rock"
+                        playerChoice = .rock
                     } label: {
                         VStack {
-                            Image(systemName: playerChoice == "rock" ? "circle.fill" : "circle")
+                            Image(systemName: playerChoice == .rock ? "circle.fill" : "circle")
                             Text("Rock")
                         }
                     }
                     .frame(width: 100, height: 75)
                     .background(.red)
                     .cornerRadius(30)
-                    .fontWeight(playerChoice == "rock" ? .bold : .regular)
+                    .fontWeight(playerChoice == .rock ? .bold : .regular)
                 }
                 
-                if !choiceIsFinalised || playerChoice == "paper" {
+                if event == .picking || playerChoice == .paper {
                     Button() {
-                        playerChoice = "paper"
+                        playerChoice = .paper
                     } label: {
                         VStack {
-                            Image(systemName: playerChoice == "paper" ? "square.fill" : "square")
+                            Image(systemName: playerChoice == .paper ? "square.fill" : "square")
                             Text("Paper")
                         }
                     }
                     .frame(width: 100, height: 75)
                     .background(.yellow)
                     .cornerRadius(30)
-                    .fontWeight(playerChoice == "paper" ? .bold : .regular)
+                    .fontWeight(playerChoice == .paper ? .bold : .regular)
                 }
                 
-                if !choiceIsFinalised || playerChoice == "scissors" {
+                if event == .picking || playerChoice == .scissors {
                     Button() {
-                        playerChoice = "scissors"
+                        playerChoice = .scissors
                     } label: {
                         VStack {
-                            Image(systemName: playerChoice == "scissors" ? "triangle.fill" : "triangle")
+                            Image(systemName: playerChoice == .scissors ? "triangle.fill" : "triangle")
                             Text("Scissors")
                         }
                     }
                     .frame(width: 100, height: 75)
                     .background(.blue)
                     .cornerRadius(30)
-                    .fontWeight(playerChoice == "scissors" ? .bold : .regular)
+                    .fontWeight(playerChoice == .scissors ? .bold : .regular)
                 }
             }
             .font(.title3)
             .foregroundColor(.white)
             Button() {
                 withAnimation() {
-                    choiceIsFinalised.toggle()
+                    switch event {
+                    case .picking:
+                        if playerChoice != .none {
+                            switch Int.random(in: 0...2) {
+                            case 0:
+                                comChoice = .rock
+                                switch playerChoice {
+                                case .rock:
+                                    event = .draw
+                                case .paper:
+                                    event = .win
+                                default:
+                                    event = .lose
+                                }
+                            case 1:
+                                comChoice = .paper
+                                switch playerChoice {
+                                case .rock:
+                                    event = .lose
+                                case .paper:
+                                    event = .draw
+                                default:
+                                    event = .win
+                                }
+                            default:
+                                comChoice = .scissors
+                                switch playerChoice {
+                                case .rock:
+                                    event = .win
+                                case .paper:
+                                    event = .lose
+                                default:
+                                    event = .draw
+                                }
+                            }
+                        }
+                    default:
+                        event = .picking
+                        playerChoice = .none
+                        comChoice = .none
+                    }
+                    buttonText = event == .picking ? "I'm Ready" : "Reset"
+                
                 }
             } label: {
-                Text("I'm Ready")
+                Text(buttonText)
                     .frame(width: 250, height: 50)
-                    .background(.green)
+                    .background(playerChoice == .none || event != .picking ? .orange : .green)
                     .foregroundColor(.white)
                     .cornerRadius(20)
                     .font(.title2)
-                    .fontWeight(.semibold)
+                    .fontWeight(playerChoice == .none || event != .picking ? .regular : .bold)
                     .padding(5)
             }
         }
